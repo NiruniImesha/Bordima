1b9990fcb197e25361a80690c4bf66e29e7e64eb
Niruni Imesha <niruniimesha@gmail.com>
2022-05-08 16:56:14 +0530

    Add Services insert

--- app/src/main/java/com/projectmad/bordima/AddServiceActivity.java
+++ app/src/main/java/com/projectmad/bordima/AddServiceActivity.java
@@ -3,6 +3,7 @@ package com.projectmad.bordima;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
@@ -21,14 +22,11 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -36,36 +34,203 @@ import java.util.HashMap;
 
 public class AddServiceActivity extends AppCompatActivity {
 
-    private String categoryName;
+    private String CategoryName, Description, ContactNo, ServiceName,Location, saveCurrentDate, saveCurrentTime;
     private Button nextbtn;
-    private EditText InputName, InputCategory,InputDescription, InputLocation, InputContactNo;
-    private ImageView InputCoverphoto;
-    private ProgressDialog loadingbar;
+    private ImageView InputServiceImage;
+    private EditText InputServiceName, InputServiceDescription, InputContactNo, InputLocation;
+    private static final int GalleryPick = 1;
+    private Uri ImageUri;
+    private String ServiceRandomKey, downloadImageUrl;
+    private StorageReference ServiceImagesRef;
+    private DatabaseReference ServiceRef;
+    private ProgressDialog loadingBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_service);
 
-        categoryName = getIntent().getExtras().get("category").toString();
+        CategoryName = getIntent().getExtras().get("category").toString();
+        ServiceImagesRef = FirebaseStorage.getInstance().getReference().child("Product Images");
+        ServiceRef = FirebaseDatabase.getInstance().getReference().child("Products");
 
-        Toast.makeText(this, categoryName, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, CategoryName, Toast.LENGTH_SHORT).show();
 
         nextbtn =(Button) findViewById(R.id.ServiceNext);
-        InputName = (EditText) findViewById(R.id.ServiceName);
+        InputServiceName = (EditText) findViewById(R.id.ServiceName);
         InputContactNo = (EditText) findViewById(R.id.ServiceContactNo);
-        //InputCoverphoto = (ImageView) findViewById(R.id.ServiceCoverPhoto);
-        InputDescription = (EditText) findViewById(R.id.ServiceDdescription);
+        InputServiceImage= (ImageView) findViewById(R.id.ServiceImageBtn);
+        InputServiceDescription = (EditText) findViewById(R.id.ServiceDdescription);
         InputLocation = (EditText) findViewById(R.id.ServiceLocation);
-        loadingbar = new ProgressDialog(this);
+        loadingBar = new ProgressDialog(this);
+
+        InputServiceImage.setOnClickListener(view -> OpenGallery());
+
 
         nextbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AddServiceActivity.this,AddPackageActivity.class);
-                startActivity(intent);
+            public void onClick(View view)
+            {
+                ValidateProductData();
             }
         });
+    }
 
+    private void OpenGallery()
+    {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, GalleryPick);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==GalleryPick  &&  resultCode==RESULT_OK  &&  data!=null)
+        {
+            ImageUri = data.getData();
+            InputServiceImage.setImageURI(ImageUri);
+        }
+    }
+
+
+    private void ValidateProductData()
+    {
+        Description = InputServiceDescription.getText().toString();
+        ServiceName = InputServiceName.getText().toString();
+        ContactNo = InputContactNo.getText().toString();
+        Location = InputLocation.getText().toString();
+
+        if (ImageUri == null)
+        {
+            Toast.makeText(this, "Product image is mandatory...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(Description))
+        {
+            Toast.makeText(this, "Please write service description...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(ServiceName))
+        {
+            Toast.makeText(this, "Please enter service name...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(Location))
+        {
+            Toast.makeText(this, "Please enter location...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            StoreServiceInformation();
+        }
+    }
+
+
+
+    private void StoreServiceInformation()
+    {
+        loadingBar.setTitle("Add New Product");
+        loadingBar.setMessage("Dear Admin, please wait while we are adding the new product.");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+
+        Calendar calendar = Calendar.getInstance();
+
+        SimpleDateFormat currentDate = new SimpleDateFormat("MMM dd, yyyy");
+        saveCurrentDate = currentDate.format(calendar.getTime());
+
+        SimpleDateFormat currentTime = new SimpleDateFormat("HH:mm:ss a");
+        saveCurrentTime = currentTime.format(calendar.getTime());
+
+        ServiceRandomKey = saveCurrentDate + saveCurrentTime;
+
+
+        final StorageReference filePath = ServiceImagesRef.child(ImageUri.getLastPathSegment() + ServiceRandomKey + ".jpg");
+
+        final UploadTask uploadTask = filePath.putFile(ImageUri);
+
+
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e)
+            {
+                String message = e.toString();
+                Toast.makeText(AddServiceActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                loadingBar.dismiss();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot)
+            {
+                Toast.makeText(AddServiceActivity.this, "Product Image uploaded Successfully...", Toast.LENGTH_SHORT).show();
+
+                Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception
+                    {
+                        if (!task.isSuccessful())
+                        {
+                            throw task.getException();
+                        }
+
+                        downloadImageUrl = filePath.getDownloadUrl().toString();
+                        return filePath.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            downloadImageUrl = task.getResult().toString();
+
+                            Toast.makeText(AddServiceActivity.this, "got the Product image Url Successfully...", Toast.LENGTH_SHORT).show();
+
+                            SaveProductInfoToDatabase();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+
+
+    private void SaveProductInfoToDatabase()
+    {
+        HashMap<String, Object> productMap = new HashMap<>();
+        productMap.put("pid", ServiceRandomKey);
+        productMap.put("date", saveCurrentDate);
+        productMap.put("time", saveCurrentTime);
+        productMap.put("sname", ServiceName);
+        productMap.put("category", CategoryName);
+        productMap.put("simage", downloadImageUrl);
+        productMap.put("sdescription", Description);
+        productMap.put("location", Location);
+        productMap.put("contactno", ContactNo);
+
+        ServiceRef.child(ServiceRandomKey).updateChildren(productMap)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            Intent intent = new Intent(AddServiceActivity.this, AddPackageActivity.class);
+                            startActivity(intent);
+
+                            loadingBar.dismiss();
+                            Toast.makeText(AddServiceActivity.this, "Product is added successfully..", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            loadingBar.dismiss();
+                            String message = task.getException().toString();
+                            Toast.makeText(AddServiceActivity.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
