29a6408388d057e55f6952bd81a6d4a710a38413
ShehaniHettiarachchi <88665593+ShehaniHettiarachchi@users.noreply.github.com>
2022-05-14 14:44:59 +0530

    Commit welcome UI

--- app/src/main/java/com/projectmad/bordima/Login_activity.java
+++ app/src/main/java/com/projectmad/bordima/Login_activity.java
@@ -5,12 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class Dashboard extends AppCompatActivity {
+public class Login_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_login);
     }
 }
