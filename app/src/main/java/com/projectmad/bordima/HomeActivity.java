c808fd2ac7675749ef33c32d9840eb0b2196123c
ShehaniHettiarachchi <88665593+ShehaniHettiarachchi@users.noreply.github.com>
2022-05-17 14:32:02 +0530

    Commit Feedback_recyclerview

--- app/src/main/java/com/projectmad/bordima/HomeActivity.java
+++ app/src/main/java/com/projectmad/bordima/HomeActivity.java
@@ -10,14 +10,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
-import com.google.android.play.core.review.ReviewManagerFactory;
-import com.google.android.play.core.tasks.OnCompleteListener;
-import com.google.android.play.core.tasks.OnSuccessListener;
-import com.google.android.play.core.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,7 +21,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.projectmad.bordima.Model.Feedback;
 
 import java.util.ArrayList;
-
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -38,124 +32,71 @@ public class HomeActivity extends AppCompatActivity {
     ArrayList<Feedback> FeedbackArrayList;
     //Feedback list
 
-    private Button logout_btn, review_btn,feedback;
-
-    private ReviewManager manager;
-    private ReviewInfo reviewInfo;
+    private Button logout_btn,feedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        activeReviewInfo();
+            logout_btn = (Button) findViewById(R.id.logout_btn);
+            feedback = (Button) findViewById(R.id.feedback);
 
-        logout_btn = (Button) findViewById(R.id.logout_btn);
-        review_btn = (Button) findViewById(R.id.rating);
-        feedback = (Button) findViewById(R.id.feedback);
+            recyclerView = findViewById(R.id.FeedbackList);
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        recyclerView = findViewById(R.id.FeedbackList);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            firebaseDatabase = FirebaseDatabase.getInstance();
+            RootRef = firebaseDatabase.getReference("Feedbacks");
+            FeedbackArrayList = new ArrayList<Feedback>();
+            feedbackAdapter = new FeedbackAdapter(HomeActivity.this,FeedbackArrayList);
+            recyclerView.setAdapter(feedbackAdapter);
 
-        firebaseDatabase = FirebaseDatabase.getInstance();
-        RootRef = firebaseDatabase.getReference("Feedbacks");
-        FeedbackArrayList = new ArrayList<Feedback>();
-        feedbackAdapter = new FeedbackAdapter(HomeActivity.this,FeedbackArrayList);
-        recyclerView.setAdapter(feedbackAdapter);
+            RootRef.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                    Feedback feedback = snapshot.getValue(Feedback.class);
+                    FeedbackArrayList.add(feedback);
+                    feedbackAdapter.notifyDataSetChanged();
+                }
 
-        RootRef.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                Feedback feedback = snapshot.getValue(Feedback.class);
-                FeedbackArrayList.add(feedback);
-                feedbackAdapter.notifyDataSetChanged();
-            }
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                }
 
-            }
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
 
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                }
 
-            }
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
 
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                }
 
-            }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+                }
 
-            }
+            });
 
+            logout_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
 
-
-
-        });
-
-
-
-
-
-
-        review_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startReviewFlow();
-            }
-        });
-
-        logout_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        feedback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(HomeActivity.this, WriteReviewActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-
-
-
-    void activeReviewInfo()
-    {
-        manager = ReviewManagerFactory.create(this);
-        Task<ReviewInfo> managerInfoTask = manager.requestReviewFlow();
-
-        managerInfoTask.addOnSuccessListener((task)->
-        {
-            if (managerInfoTask.isSuccessful())
-            {
-                reviewInfo = managerInfoTask.getResult();
-            }
-            else
-            {
-                Toast.makeText(this, "Review failed to start", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    void startReviewFlow()
-    {
-        if (reviewInfo != null)
-        {
-            Task<Void> flow = manager.launchReviewFlow(this, reviewInfo);
-            flow.addOnSuccessListener(task->
-            {
-                Toast.makeText(this, "Rating is completed", Toast.LENGTH_SHORT).show();
+            feedback.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(HomeActivity.this, WriteReviewActivity.class);
+                    startActivity(intent);
+                }
             });
         }
-    }
+
 }
